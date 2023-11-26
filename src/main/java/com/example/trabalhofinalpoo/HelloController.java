964ad2a049bac100b0122955c7a61936a9ffd939
Guilherme Alvarenga <guilherme.azevedo@pdcase.com.br>
2023-11-26 10:39:47 -0300

    first commit

---
+++ src/main/java/com/example/trabalhofinalpoo/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.trabalhofinalpoo;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Ui");
+    }
+}
